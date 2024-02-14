074381c2c71bf48fe10ea7e396df16963c556b57
Jamie Stadnik <jstadni2@illinois.edu>
2024-02-14 09:42:35 -0600

    Remove extra whitespace in TestClassDemoInstance

--- tests/2_creating_tests/test_math_operations.py
+++ tests/2_creating_tests/test_math_operations.py
@@ -27,9 +27,9 @@ class TestClassDemoInstance:
     y = 10
     def test_one(self):
         self.x = 10
-        assert add_numbers(self.x,  self.y) == 20
+        assert add_numbers(self.x, self.y) == 20
     
     # Each test has a unique instance of the class
     def test_two(self):
-        assert add_numbers( self.x,  self.y) == 15
+        assert add_numbers(self.x, self.y) == 15
         assert self.x == 5
