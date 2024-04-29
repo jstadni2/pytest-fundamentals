3a65caa285d949def45118d3446a8f0976db6579
Jamie Stadnik <jstadni2@illinois.edu>
2024-04-29 14:41:39 -0500

    Pass test_get_mocked()

---
+++ tests/8_mocking/test_mock.py
@@ -0,0 +1,1 @@
+# TODO: Use MagicMock in this example
--- tests/8_mocking/test_monkeypatch.py
+++ tests/8_mocking/test_monkeypatch.py
@@ -17,11 +17,41 @@ def test_get(api_client):
 
     # Confirm that the request-response cycle completed successfully
     assert response.ok
+
+
+# TODO: Instantiate with different status codes
+# 200, 202, 400, 401, 404, 500
+# custom class to be the mock return value of requests.get()
+class MockResponse:
+    def __init__(self, status_code):
+        self.status_code = status_code
+    
+    def ok(self):
+        if self.status_code < 400:
+            return True
+        else:
+            return False
+    
+    @staticmethod
+    def json():
+        return {"mock_key": "mock_response"}
+
+
+# monkeypatched requests.get moved to a fixture
+@pytest.fixture
+def mock_client(monkeypatch):
+    """APIClient.get() mocked to return {'mock_key':'mock_response'}."""
+
+    def mock_get(*args, **kwargs):
+        return MockResponse(200)
+
+    monkeypatch.setattr(APIClient, "get", mock_get)
     
 
-def test_get_mocked(api_client, monkeypatch):
+def test_get_mocked(mock_client):
     # Send a request to the API server and store the response
+    api_client = APIClient()
     response = api_client.get()
 
     # Confirm that the request-response cycle completed successfully
-    assert response.ok
+    assert response.ok()
